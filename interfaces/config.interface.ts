dc7c6bc6b0efee46cff51f8f88a327b4beb05aac
Oleg Mavlyutov <krallakane@gmail.com>
2022-10-09 15:24:32 +0300

    feat: - добавил описание в интерфейсе конфигурации

--- interfaces/config.interface.ts
+++ interfaces/config.interface.ts
@@ -1,12 +1,43 @@
 export interface ConfigInterface {
+    /**
+     * URI адрес Redis - пример в .env-example
+     */
     redisURI: string;
 
+
+    /**
+     * URL адрес clickhouse
+     */
     clickhouseURL: string;
+
+    /**
+     * Порт clickhouse
+     */
     clickhousePort: number;
+
+    /**
+     * Название базы в clickhouse
+     */
     clickhouseDatabase: string;
+
+    /**
+     * Имя для авторизации в clickhouse
+     */
     clickhouseUsername: string;
+
+    /**
+     * Пароль для авторизации в clickhouse
+     */
     clickhousePassword: string;
 
+
+    /**
+     * Максимальный размер буфера
+     */
     bufferMaxSize: number;
+
+    /**
+     * Максимальное время перед записью из буфера в секундах
+     */
     bufferMaxTimeout: number;
 }
